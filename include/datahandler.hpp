b7318216982ab864babbe09088be228986c9143b
Mendel Dobondi-Reisz <dobondireiszmendel@gmail.com>
2024-09-19 14:58:56 +0200

    Added const values to datahandler.hpp

--- include/datahandler.hpp
+++ include/datahandler.hpp
@@ -12,4 +12,27 @@
 #ifndef INCLUDE_DATA_HANDLER_HPP
 #define INCLUDE_DATA_HANDLER_HPP
 
+#include <nlohmann/json.hpp>
+#include <curl/curl.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <filesystem>
+
+// ========================================================== API ONLINE COMMUNICATIONS ==========================================================
+
+#ifdef _WIN32
+    #define folderDelimiter "\\"
+#else
+    #define folderDelimiter "/"
+#endif
+
+const std::string baseURL = "https://www.alphavantage.co/query?";
+const std::string baseFilePath = "data";
+const std::string functionURL = "function=";
+const std::string symbolURL = "&symbol=";
+const std::string apikeyURL = "&apikey=";
+const std::string apiKeyPath = "data" + std::string(folderDelimiter) + ".apikey";
+
 #endif
